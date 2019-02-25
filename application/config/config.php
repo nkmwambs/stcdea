628a6eabb7cad7d3c3cb6fb1b2b75a601fc3a316
nkmwambs <nkmwambs@gmail.com>
2019-02-26 00:04:46 +0300

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -17,7 +17,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | environments.
 |
 */
-$config['base_url'] = 'http://localhost/stcdea/';
+$config['base_url'] = 'http://bvamanager.org/stcdea/';
 
 /*
 |--------------------------------------------------------------------------
